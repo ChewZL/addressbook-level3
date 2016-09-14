7cf87c05ae16c6698e3478ca9e853c36a6351355
ChewZL <chewzl@yahoo.com.sg>
2016-09-14 10:32:14 +0800

    Use Polymorphism and LSP

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,7 +39,12 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws Exception;
+    
+    /**
+     * Return true if the command mutates data
+     */
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
